cfcd36e19342b05b8efafae60b7406824eee4694
Filip Tatíček <filtaticek@seznam.cz>
2023-04-14 17:42:44 +0200

    Removing old backend, starting with new backend

--- app/components/Movie.tsx
+++ app/components/Movie.tsx
@@ -2,16 +2,16 @@ import Link from "next/link"
 import Image from "next/image"
 
 export default function Movie ({title, id, posterPath, releaseDate}:any) {
-    const imagePath = "https://image.tmdb.org/t/p/original"
+  const imagePath = "https://image.tmdb.org/t/p/original"
 
-    return(
-        <div className="mb-10 w-[20%] text-center" key={id}>
-            <h1 className="font-bold">{title}</h1>
-            <p>{releaseDate}</p>
-            <Link href={id.toString()}>
-                {<Image className="m-auto" src={imagePath+posterPath} width={200} height={400} alt="" />}
-            </Link>
+  return(
+    <div className="mb-10 w-[20%] text-center" key={id}>
+      <h1 className="font-bold">{title}</h1>
+      <p>{releaseDate}</p>
+      <Link href={`/movie/${id.toString()}`}>
+        {<Image className="m-auto h-[400px] w-[200px]" width={200} height={300} src={imagePath+posterPath} alt="" />}
+      </Link>
 
-        </div>
-    )
+    </div>
+  )
 }
