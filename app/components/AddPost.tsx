fbc4da9e80438889fa041b4b1b1aee2e307957ef
Filip Tatíček <filtaticek@seznam.cz>
2023-04-17 12:59:08 +0200

    removing css file, installing tanstack, some changes on main page

--- app/components/AddPost.tsx
+++ app/components/AddPost.tsx
@@ -6,16 +6,35 @@ export default function AddPost () {
 
   const [title, setTitle] = useState("")
 
+  const onSubmit = (e:any) => {
+    e.preventDefault()
+    console.log(title)
+  }
+
   return(
     <form>
       <div>
-        <textarea 
-          onChange={(e) =>setTitle(e.target.value)} 
-          className="bg-gray-200 rounded-md p-8 border border-black"
-          name="title" 
-          value={title} 
-          placeholder="What's your on your mind ?"
-        />
+        <div className="p-5">
+          <textarea 
+            maxLength={300}
+            onChange={(e) =>setTitle(e.target.value)} 
+            className="w-full rounded-md border border-black bg-gray-200 p-8 placeholder:text-black"
+            name="title" 
+            value={title} 
+            placeholder="What's your on your mind ?"
+          />
+          <div className="flex items-center justify-between">
+            <p className="font-bold">{title.length}/300</p>
+            <button 
+              type="submit"
+              className="my-2 rounded-md bg-green-600 px-5 py-2 text-white"
+              disabled={title === ""}
+              onSubmit={onSubmit}
+            >
+            Submit
+            </button> 
+          </div>
+        </div>
       </div>
     </form>
   )
