5a336811cf1d106e948e43653e72e1cab207f0f2
Filip Tatíček <filtaticek@seznam.cz>
2023-04-29 14:52:38 +0200

    fixing hot toast

--- src/server/components/AddPost.tsx
+++ src/server/components/AddPost.tsx
@@ -5,26 +5,29 @@ import { useMutation } from "@tanstack/react-query"
 import axios, { AxiosError } from "axios"
 import toast from "react-hot-toast"
 import { useQueryClient } from "@tanstack/react-query"
+import { useRef } from "react"
 
 export default function AddPost () {
 
   const [title, setTitle] = useState("")
   const [disabled, setDisabled] = useState(false)
-  const toastPostID = ""
   const queryClient = useQueryClient()
+  const toastPostID = useRef("")
 
   const { mutate } = useMutation(
     async (title: string) => await axios.post("/api/post/addPost", { title }),
     {
       onError: (error:any) => {
         if (error instanceof AxiosError) {
-          toast.error("Something went wrong", {id: toastPostID})
+          toast.remove(toastPostID.current)
+          toast.error("Something went wrong")
           console.log("Error sending post:", error.response?.data)
         }
         setDisabled(false)
       },
       onSuccess: (data) => {
-        toast.success("Post has been made! ", {id: toastPostID})
+        toast.remove(toastPostID.current)
+        toast.success("Post has been made! ")
         console.log("Post succesful: ", data.data)
         setDisabled(false)
         queryClient.invalidateQueries(["allPosts"]) //if we add new post, the old query is refetched
@@ -33,10 +36,10 @@ export default function AddPost () {
   )
 
   async function submitPost (e:React.FormEvent) {
+    toastPostID.current = toast.loading("Creating your post...")  
     e.preventDefault()
     setTitle("")
     setDisabled(true)
-    //toast.loading("Creating your post...", {id: toastPostID})  
     mutate(title)
   }
 
